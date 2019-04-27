f6caf56639193954d22ab0ecd775a390b276f939
GdnCool <31330256+GdnCool@users.noreply.github.com>
2019-04-28 02:45:35 +0800

    Add files via upload

---
+++ Target.vue
@@ -0,0 +1,35 @@
+<template>
+    
+<div id="Target">
+        健身类型&#160;<Input v-model="formItem.goal_type" placeholder="健身类型" style="width: 300px" />
+        <br>
+        目标层次&#160;<Input v-model="formItem.goal" placeholder="目标层次" style="width: 300px" />
+        <br>
+        <i-button type="button">确定</i-button>
+    </div>
+</template>
+<script>
+import Vue from 'vue'
+export default {
+    data() {
+      return {
+        formItem: {
+          goal_type: '',
+          goal: ''
+        }
+      }
+    },
+    methods: {
+      register() {
+        this.axios.post('http://n828vd.natappfree.cc/user/insertGoal', {
+            goal_type: this.formItem.goal_type,
+            goal: this.formItem.goal
+          })
+          .then(res => console.log(res))
+          .catch(err => console.log(err));
+      }
+    }
+  }
+</script>
+<style>
+</style>
